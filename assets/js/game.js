c823a130e734dc0b1c9ef21c0e0002bd5dba5e50
Jake Cheatham <jakelliot98@gmail.com>
2021-10-26 22:03:40 -0400

    cleaned up code, only option to skip now, added player reward for winning, deduction for skipping

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,85 +7,96 @@ function fight () {
 //A variable is a named location for a value that gets stored in the browser's memory when a program is run.
 
 //CLOSE EVERY LINE OF CODE WITH ;!!!!!!!!!
+console.log("apple");
+
 
 var playerName = window.prompt ("What is your robots name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
+console.log(playerName, playerHealth)
+
 var enemyNames = ["Balthazar", "Roberto", "Karen"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 
-
-var fight = function(enemyName) {
-    //tells players fight has begun
 window.alert ("Welcome to Robot Gladiators");
+var fight = function(enemyName) {
+    while(playerHealth > 0 && enemyHealth > 0) {
+        
+        var promptFight = window.prompt ("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
 
-var promptFight = window.prompt ("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
-console.log (promptFight)
+        //if player chooses to skip
+        if (promptFight === "SKIP" || promptFight === "skip") {
+            // confirm player skip
+            var confirmSkip = window.confirm ("Are you sure you would like to quit?")
+             
+            if (confirmSkip) {
+                window.alert (playerName + " has decided to skip this fight! Goodbye.");
+            //subtract money for skipping
+            playerMoney = playerMoney - 10;
+            console.log("playerMoney" + playerMoney);
+            break;
+            }
+    
+            }
+            
+        
+            //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+            //Listing the variable on the left side means we'll store data to that variable, and listing the variable on the right side means we'll use the actual value that variable holds at that moment.
+        
+        enemyHealth = enemyHealth - playerAttack;
+        
+        // Log a resulting message to the console so we know that it worked.
+        
+        console.log(
+            playerName + " has attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            )
+        
+        //check enemy health
+        
+        if (enemyHealth <= 0) {
+            window.alert (enemyName + " has died!");
+            playerMoney = playerMoney + 20;
+            break;
+        }
+        
+        else {
+            window.alert (enemyName + " still has " + enemyHealth + " health left!");
+        }    
+            // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+        
+        playerHealth = playerHealth - enemyAttack;  
+        
+            // Log a resulting message to the console so we know that it worked.
+        
+        console.log(enemyName + " has attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
+        
+        
+        //check player health
+        
+        if (playerHealth <= 0) {
+            window.alert (playerName + " has died!");
+            break;
+        }
+        else {
+            window.alert (playerName + " still has " + playerHealth + " health left.");
+        }
+        }
+        //closing bracket on fight function...SUPER IMPORTANT KEEP EVERYTHING INSIDE THIS
+    }
 
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    //Listing the variable on the left side means we'll store data to that variable, and listing the variable on the right side means we'll use the actual value that variable holds at that moment.
-if (promptFight === "FIGHT" || promptFight === "fight") {
-enemyHealth = enemyHealth - playerAttack;
-
-// Log a resulting message to the console so we know that it worked.
-
-console.log(playerName + " has attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-
-//check enemy health
-
-if (enemyHealth <= 0) {
-    window.alert (enemyName + " has died!");
-}
-
-else {
-    window.alert (enemyName + " still has " + enemyHealth + " health left!");
-}    
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-
-playerHealth = playerHealth - enemyAttack  
-
-    // Log a resulting message to the console so we know that it worked.
-
-console.log(enemyName + " has attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    
+    //tells players fight has begun
 
 
-//check player health
 
-if (playerHealth <= 0) {
-    window.alert (playerName + " has died!");
-}
-else {
-    window.alert (playerName + " still has " + playerHealth + " health left.");
-}
-}
-//if player chooses to skip
-else if (promptFight === "SKIP" || promptFight === "skip") {
-
-// confirm player skip
-var confirmSkip = window.confirm ("Are you sure you would like to quit?")
- 
-if (confirmSkip) {
-    window.alert (playerName + " has decided to skip this fight! Goodbye.");
-//subtract money for skipping
-playerMoney = playerMoney - 2;
-}
-//if no skip
-else {
-    fight();
-}
-}
-
-else {
-    window.alert ("You need to choose a valid option. Try again!");
-}
-//closing bracket on fight function...SUPER IMPORTANT KEEP EVERYTHING INSIDE THIS
-}
 
 //RUNS FIGHT FUNCTION
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
